e68d981d7d5257ca28f2264aa5209a8928f39fc2
manichandan <manichandan888@gmail.com>
2026-08-22 09:02:10 +0530

    Create movie model

---
+++ lib/models/movie.dart
@@ -0,0 +1,19 @@
+class Movie {
+  final int id;
+  final String title;
+  final String posterUrl;
+  final String description;
+  final double rating;
+  final String genre;
+  final String releaseDate;
+
+  Movie({
+    required this.id,
+    required this.title,
+    required this.posterUrl,
+    required this.description,
+    required this.rating,
+    required this.genre,
+    required this.releaseDate,
+  });
+}
