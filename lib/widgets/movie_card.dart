b0befa91382f2b3bfc678411f659861adc98bfd1
manichandan <manichandan888@gmail.com>
2026-09-23 23:16:50 +0530

    Add movie card hover animation

--- lib/widgets/movie_card.dart
+++ lib/widgets/movie_card.dart
@@ -6,7 +6,7 @@ import '../providers/favorite_provider.dart';
 import '../screens/movie_details_screen.dart';
 import 'rating_badge.dart';
 
-class MovieCard extends StatelessWidget {
+class MovieCard extends StatefulWidget {
   final Movie movie;
 
   const MovieCard({
@@ -15,111 +15,142 @@ class MovieCard extends StatelessWidget {
   });
 
   @override
+  State<MovieCard> createState() => _MovieCardState();
+}
+
+class _MovieCardState extends State<MovieCard> {
+  bool isHovered = false;
+
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => MovieDetailsScreen(
-              movie: movie,
-            ),
-          ),
-        );
+    return MouseRegion(
+      onEnter: (_) {
+        setState(() {
+          isHovered = true;
+        });
       },
-      child: Card(
-        elevation: 4,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Movie Poster and Rating
-            Expanded(
-              child: Stack(
-                children: [
-                  ClipRRect(
-                    borderRadius: const BorderRadius.vertical(
-                      top: Radius.circular(12),
-                    ),
-                    child: Image.network(
-                      movie.posterUrl,
-                      width: double.infinity,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-
-                  // Reusable Rating Badge
-                  Positioned(
-                    top: 8,
-                    right: 8,
-                    child: RatingBadge(
-                      rating: movie.rating,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-
-            // Movie Title
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Text(
-                movie.title,
-                style: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
+      onExit: (_) {
+        setState(() {
+          isHovered = false;
+        });
+      },
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 250),
+        curve: Curves.easeInOut,
+        transform: Matrix4.identity()
+  ..scaleByDouble(
+    isHovered ? 1.02 : 1.0,
+    isHovered ? 1.02 : 1.0,
+    1.0,
+    1.0,
+  ),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => MovieDetailsScreen(
+                  movie: widget.movie,
                 ),
               ),
+            );
+          },
+          child: Card(
+            elevation: isHovered ? 8 : 4,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
             ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Stack(
+                    children: [
+                      ClipRRect(
+                        borderRadius: const BorderRadius.vertical(
+                          top: Radius.circular(12),
+                        ),
+                        child: Image.network(
+                          widget.movie.posterUrl,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
 
-            // Genre and Favorite Button
-            Padding(
-              padding: const EdgeInsets.fromLTRB(
-                10,
-                0,
-                6,
-                10,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    movie.genre,
-                    style: TextStyle(
-                      color: Colors.grey[700],
+                      Positioned(
+                        top: 8,
+                        right: 8,
+                        child: RatingBadge(
+                          rating: widget.movie.rating,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    widget.movie.title,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
+                ),
 
-                  Consumer<FavoriteProvider>(
-                    builder: (
-                      context,
-                      favoriteProvider,
-                      child,
-                    ) {
-                      final isFavorite =
-                          favoriteProvider.isFavorite(movie);
-
-                      return IconButton(
-                        onPressed: () {
-                          favoriteProvider.toggleFavorite(movie);
-                        },
-                        icon: Icon(
-                          isFavorite
-                              ? Icons.favorite
-                              : Icons.favorite_border,
-                          color: isFavorite
-                              ? Colors.red
-                              : null,
-                        ),
-                      );
-                    },
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(
+                    10,
+                    0,
+                    6,
+                    10,
                   ),
-                ],
-              ),
+                  child: Row(
+                    mainAxisAlignment:
+                        MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        widget.movie.genre,
+                        style: TextStyle(
+                          color: Colors.grey[700],
+                        ),
+                      ),
+
+                      Consumer<FavoriteProvider>(
+                        builder: (
+                          context,
+                          favoriteProvider,
+                          child,
+                        ) {
+                          final isFavorite =
+                              favoriteProvider.isFavorite(
+                            widget.movie,
+                          );
+
+                          return IconButton(
+                            onPressed: () {
+                              favoriteProvider.toggleFavorite(
+                                widget.movie,
+                              );
+                            },
+                            icon: Icon(
+                              isFavorite
+                                  ? Icons.favorite
+                                  : Icons.favorite_border,
+                              color: isFavorite
+                                  ? Colors.red
+                                  : null,
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
